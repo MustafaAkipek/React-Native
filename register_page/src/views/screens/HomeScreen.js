f5001e48c93b25846f6a4d033d31cf8678036cbb
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-12-17 08:24:56 +0300

    .

--- register_page/src/views/screens/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'; // veri depolamak için kullanılıyor
-import React from 'react'; // ana kütüphane
-import {View} from 'react-native'; // uygulamanın arayüzünde kullanılan temel bir bileşen
-
-const HomeScreen = ({navigation}) => { // prop(navigation) ile ekranlar arası gezinmeyi sağlayan bir navigasyon nesnesini temsil ediyor
-    return <View></View>;
-};
-
-export default HomeScreen; // bileşeni başka dosyalarda da kullanmaya imkan sağlıyor
